e543e216da784eed3ff348abd90b085733177ec6
Mark Dessain <mark.dessain@gmail.com>
2016-06-17 23:59:15 +0100

    update config

--- python/config.py
+++ python/config.py
@@ -3,5 +3,16 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
+LOG_LEVEL = os.environ['LOG_LEVEL']
+FLASK_DEBUG = bool(os.environ['LOG_LEVEL'] == 'debug')
+
+LOCAL = os.environ['LOCAL'].lower() == 'true'
+
 TWITTER_CONSUMER_TOKEN = os.environ['TWITTER_CONSUMER_TOKEN']
 TWITTER_CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+S3_BUCKET = os.environ['S3_BUCKET']
+S3_DATA = os.environ['S3_DATA']
